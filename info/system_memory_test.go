5ff7af38fa9646a3a4c227281ae84febfe1e1af1
bulsond <bulsond@gmail.com>
2026-02-08 15:59:32 +0300

    step: SystemMemoryInfo String()

--- info/system_memory_test.go
+++ info/system_memory_test.go
@@ -1,17 +1,20 @@
 package info
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestSystemMemoryInfo(t *testing.T) {
+	smi := SystemMemoryInfo{
+		TotalMemory:     16 * 1024 * 1024 * 1024, // 16 GB
+		FreeMemory:      4 * 1024 * 1024 * 1024,  // 4 GB
+		AvailableMemory: 6 * 1024 * 1024 * 1024,  // 6 GB
+		SwapTotal:       8 * 1024 * 1024 * 1024,  // 8 GB
+		SwapFree:        7 * 1024 * 1024 * 1024,  // 7 GB
+	}
 	t.Run("Успешное создание экземпляра SystemMemoryInfo",
 		func(t *testing.T) {
-			want := SystemMemoryInfo{
-				TotalMemory:     16 * 1024 * 1024 * 1024, // 16 GB
-				FreeMemory:      4 * 1024 * 1024 * 1024,  // 4 GB
-				AvailableMemory: 6 * 1024 * 1024 * 1024,  // 6 GB
-				SwapTotal:       8 * 1024 * 1024 * 1024,  // 8 GB
-				SwapFree:        7 * 1024 * 1024 * 1024,  // 7 GB
-			}
 			got := SystemMemoryInfo{
 				TotalMemory:     GBytes(16),
 				FreeMemory:      GBytes(4),
@@ -19,8 +22,28 @@ func TestSystemMemoryInfo(t *testing.T) {
 				SwapTotal:       GBytes(8),
 				SwapFree:        GBytes(7),
 			}
-			if got != want {
-				t.Errorf("got %q want %q", got, want)
+			if got != smi {
+				t.Errorf("got %q want %q", got, smi)
+			}
+		})
+
+	t.Run("Правильное отображение экземпляра SystemMemoryInfo",
+		func(t *testing.T) {
+			got := smi.String()
+			if !strings.Contains(got, "System Memory:") {
+				t.Error("Не найдена строка System Memory")
+			}
+			if !strings.Contains(got, "Total:") {
+				t.Error("Не найдена строка Total")
+			}
+			if !strings.Contains(got, "Used:") {
+				t.Error("Не найдена строка Used")
+			}
+			if !strings.Contains(got, "Available:") {
+				t.Error("Не найдена строка Available")
+			}
+			if !strings.Contains(got, "Swap Used:") {
+				t.Error("Не найдена строка Swap Used")
 			}
 		})
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/bulsond/memory-analyzer/display"
 	"github.com/bulsond/memory-analyzer/info"
 )
 
@@ -11,25 +10,37 @@ func main() {
 	fmt.Println("Привет")
 
 	// Создаём тестовый набор процессов
-	processes := []info.ProcessInfo{
-		{
-			PID:         1234,
-			Name:        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-			MemoryUsage: 1024 * 1024 * 1024, // 1 GB
-		},
-		{
-			PID:         5678,
-			Name:        "/Applications/Visual Studio Code.app/Contents/MacOS/Electron",
-			MemoryUsage: 512 * 1024 * 1024, // 512 MB
-		},
-		{
-			PID:         9101,
-			Name:        "/usr/bin/ssh-agent",
-			MemoryUsage: 2 * 1024 * 1024, // 2 MB
-		},
+	// processes := []info.ProcessInfo{
+	// 	{
+	// 		PID:         1234,
+	// 		Name:        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+	// 		MemoryUsage: 1024 * 1024 * 1024, // 1 GB
+	// 	},
+	// 	{
+	// 		PID:         5678,
+	// 		Name:        "/Applications/Visual Studio Code.app/Contents/MacOS/Electron",
+	// 		MemoryUsage: 512 * 1024 * 1024, // 512 MB
+	// 	},
+	// 	{
+	// 		PID:         9101,
+	// 		Name:        "/usr/bin/ssh-agent",
+	// 		MemoryUsage: 2 * 1024 * 1024, // 2 MB
+	// 	},
+	// }
+
+	// // Форматируем и выводим таблицу
+	// fmt.Println("Process List:")
+	// fmt.Print(display.FormatTable(processes))
+
+	// Создаём тестовую информацию о системной памяти
+	sysInfo := info.SystemMemoryInfo{
+		TotalMemory:     16 * 1024 * 1024 * 1024, // 16 GB
+		FreeMemory:      4 * 1024 * 1024 * 1024,  // 4 GB
+		AvailableMemory: 6 * 1024 * 1024 * 1024,  // 6 GB
+		SwapTotal:       8 * 1024 * 1024 * 1024,  // 8 GB
+		SwapFree:        7 * 1024 * 1024 * 1024,  // 7 GB
 	}
 
-	// Форматируем и выводим таблицу
-	fmt.Println("Process List:")
-	fmt.Print(display.FormatTable(processes))
+	// Форматируем и выводим статистику
+	fmt.Print(sysInfo)
 }
